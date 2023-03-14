3053a834bc8d0b104366df3d61f153f7afc156d9
Morakinyo Joseph <97817101+Morakinyo-Joseph@users.noreply.github.com>
2023-03-14 22:08:58 +0100

    Update todo.php

--- todo.php
+++ todo.php
@@ -3,105 +3,83 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <title>To-do APP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 
 </head>
-<body class="bg-light">
 
+<body>
 
 <div class="container">
 
-    <form action="todo.php", method="post">
-        <input type="text", name="new_task", class="form-control", placeholder="Input New Task Here">
-        <br>
-        <input type="submit", value="submit", class="btn btn-success">
+    <br><br>
+    <form action="first.php", method="post">
+        <input type="text", name="task", class="form-control", placeholder="Input your task...">
+        <br><br>
+        <input type="submit", value="submit", name="submit", class="btn btn-primary">
 
-    <br>
+        <hr>
+        <h1 >Tasks</h1>
 
-    <?php
+        <?php 
 
-        $newTask = $_POST["new_task"];
-
-        if(file_exists("myjson.json")){
-            $file = "myjson.json";
-            $json_data = file_get_contents($file);
-            $php_data = json_decode($json_data, true);
+            // connect to the database
+            $server = "localhost";
+            $username = "root";
+            $password = "Moraksj03mysql$.";
+            $database = "mydb";
+            $conn = mysqli_connect($server, $username, $password, $database);
             
-            $id_counter = 0;
-            foreach ($php_data as $data){
-                $id_counter += 1;
+            // input tasks into the database
+            if ($_REQUEST["submit"]){
+                $newTask = $_POST["task"];
 
-            }
-            
-            if ($newTask != ""){
-                $task = [
-                    "id" => $id_counter,
-                    "text" => $newTask
-                ];
+                if ($newTask != "")
+                {
+                    $sql = "INSERT INTO task (text) VALUES ('$newTask')";
+                    $query = mysqli_query($conn, $sql);
 
-                $php_data[] = $task;
-                $result = json_encode($php_data);
-                file_put_contents("myjson.json", $result);
-            }
-        }
-        else{
-            if ($newTask != ""){
-
-                $task = array(
-                    [
-                        "id" => 0,
-                        "text" => $newTask
-                    ],
-
-                );
-            }
-
-            $json = json_encode($task);
-            file_put_contents("myjson.json", $json);
-        }
-    
-        echo "<br>";
-        if (file_exists("myjson.json")){
-
-            $json_file = "myjson.json";
-            $json_data = file_get_contents($json_file);
-            $php_data = json_decode($json_data);
-            
-            foreach ($php_data as $data){
-                echo $data -> text . " <button class='btn btn-success', name='done', value='$data->id'>Done</button>" ."<hr>";
-
-                
-            }
-            
-            // removing data when the done key has being clicked
-            $done_id = $_POST['done'];
-            if ($done_id !== null){
-                $json_file = 'myjson.json';
-                $json_data = file_get_contents($json_file);
-                $php_data = json_decode($json_data, true);
-
-                foreach($php_data as $data => $item){
-                    if($item['id'] == $done_id){
-                        unset($php_data[$data]);
-                        break;
+                    if ($query){
+                        echo "<p style='color: green;'>New task uploaded <br></p>";
                     }
                 }
 
             }
 
-            $json_data = json_encode(array_values($php_data));
-            file_put_contents('myjson.json', $json_data);
-        }
-    ?>
+            // display task from the database
+            $sql = "SELECT * FROM mydb.task";
+            $result = mysqli_query($conn, $sql);
 
-    </p>
+            if (mysqli_num_rows($result) > 0)
+            {
+                while ($row = mysqli_fetch_assoc($result))
+                {
+                    $number = $row['id'];
+                    echo  "<p>" . $row['text'] . "<button class='btn btn-success' value='$number', name='done' style='margin-left: 50px;'>Done</button>" . "<br>";
+                    echo "</p>";
+                }
+
+                // delete task from the database
+                $id = $_POST["done"];
+                if ($id != null)
+                {
+                    $del_sql = "DELETE FROM task WHERE id = $id";
+                    $del_result = mysqli_query($conn, $del_sql);
+                    header("Refresh: 0.5");
+                }
+            }
+
+
+
+        ?>
+
     </form>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-    
 </body>
 </html>
